197be032f70e3b54f0aa69650e2ee1f04e62d28a
Jeykboy <jeykboy@gmail.com>
2016-04-10 00:00:01 -0400

    Admin Area
    Added admin/user routes to server js

--- methodtwo/server.js
+++ methodtwo/server.js
@@ -11,3 +11,27 @@
  // start the server
  app.listen(1337);
  console.log('1337 Method Two was a succes');
+
+
+ // create routes for the admin section
+
+ // get an instance of the router
+ var adminRouter = express.Router();
+
+ // admin main page. the dashboard (http://localhost:1337/admin)
+ adminRouter.get('/', function(req, res) {
+ 	res.send('I am the dashboard!');
+ });
+
+ // users page (http://localhost:1337/admin/users)
+ adminRouter.get('/users', function(req, res) {
+ 	res.send('I show all the users!');
+ });
+
+ // posts page (http://localhost:1337/admin/posts)
+ adminRouter.get('/posts', function(req, res) {
+ 	res.send('I show all the posts!');
+ });
+
+// apply the routes to our application
+app.use('/admin',adminRouter);
